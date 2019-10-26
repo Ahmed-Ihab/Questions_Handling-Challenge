2d80a4b61ac2664631bec3155d06e2193d7e6a47
AbdelazizNassif <55716788+AbdelazizNassif@users.noreply.github.com>
2019-10-26 20:47:49 +0200

    separate application + full functionality

---
+++ questionHandl.c
@@ -0,0 +1,681 @@
+﻿/*
+ * CFile1.c
+ *
+ * Created: 26/10/2019 08:18:29 م
+ *  Author: COMPUTER SHOW
+ */ 
+#include"questionHandl.h"
+
+
+static uint8 Question[5][16]={"ascii of . = 46?","ascii of + = 43?","ascii of E = 70?", "ascii of N = 78?","ascii of B = 67?"};
+static uint8 Answer_of_Questions[5]={YES,YES,NO,YES,NO};
+
+
+	
+void Question_Handling_Task (void)
+{
+	static Question_State_t Question_State = QUESTION1;
+	static APP_STATE_t app_state = PRINT_QUESTION;
+	static uint8 quest_iterator = 0;
+	static uint8 Correct_Answer=0;
+	static uint8 correct_answer_flag = 0;
+	static uint8 wrong_answer_flag = 0;
+	uint8 Answer= 6;	//any values except 1 , 2 , 3
+	uint8 Display_choices[]="1.YES 2.NO 3.SKP";
+	uint8 score[]="yr score:";
+	uint8 right[]="CORRECT ";
+	uint8 wrong[]="WRONG ";
+	uint8 static fini = 0;
+
+	switch(Question_State)
+
+	{
+		case QUESTION1:
+		{
+			switch(app_state)
+			{
+				
+				case PRINT_QUESTION:
+				{
+					if(Question[QUESTION1][quest_iterator] != '?')
+					{
+						LCD_Data_G[0][quest_iterator]= Question[QUESTION1][quest_iterator];
+						quest_iterator++;
+					}
+					else
+					{
+						app_state = PRINT_CHOICES;
+						quest_iterator = 0;
+					}
+
+					break;
+				}
+				
+				
+				case PRINT_CHOICES:
+				{
+					if(Display_choices[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+					{
+						LCD_Data_G[1][quest_iterator] = Display_choices[quest_iterator];
+						quest_iterator++;
+					}
+
+					else								 // Answer Statement has been completely printed so it's time to choose your answer
+					{
+						quest_iterator=0;
+						app_state = GET_ANSWER;
+					}
+
+					break;
+				}
+
+
+				case GET_ANSWER:
+				{
+					if(	Keypad_Get_Data_from_Buffer(&Answer) )
+					{
+						if(Answer == Answer_of_Questions[QUESTION1] )
+						{
+							Correct_Answer++;
+							correct_answer_flag = 1;
+							wrong_answer_flag = 0;
+							LCD_Send_Byte( 0x01 ,0);
+							app_state = SHOW_CORRECT_OR_NOT;
+							
+						}
+
+						else if(Answer != Answer_of_Questions[QUESTION1] && Answer != SKIP)
+						{
+							//clear screen
+							LCD_Send_Byte( 0x01 ,0);
+							wrong_answer_flag = 1;
+							correct_answer_flag = 0;
+							app_state = SHOW_CORRECT_OR_NOT;
+						}
+						else if (Answer == SKIP)
+						{
+							//clear screen
+							LCD_Send_Byte( 0x01 ,0);
+							app_state = PRINT_QUESTION;
+							Question_State=QUESTION2;
+							//Do Nothing
+						}
+						else
+						{
+							//error not availble
+						}
+						
+						//app_state = SHOW_CORRECT_OR_NOT;
+						quest_iterator=0;
+					}
+					break;
+				}
+				case SHOW_CORRECT_OR_NOT:
+				{
+					if(correct_answer_flag == 1 )
+					{
+						//Correct_Answer++;
+						
+						if(score[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+						{
+							LCD_Data_G[0][quest_iterator] = right[quest_iterator];
+							quest_iterator++;
+						}
+						else
+						{
+							quest_iterator = 0;
+							correct_answer_flag = 0;
+							//LCD_Send_Byte( 0x01 ,0);
+							app_state = PRINT_QUESTION;
+							Question_State = QUESTION2;
+						}
+						
+						
+					}
+					else if (wrong_answer_flag == 1 )
+					{
+						if(score[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+						{
+							LCD_Data_G[0][quest_iterator] = wrong[quest_iterator];
+							quest_iterator++;
+						}
+						else
+						{
+							quest_iterator = 0;
+							wrong_answer_flag = 0;
+							//LCD_Send_Byte( 0x01 ,0);
+							app_state = PRINT_QUESTION;
+							Question_State = QUESTION2;
+						}
+					}
+					break;
+				}
+			}
+			break;
+		}
+		case QUESTION2:
+		{
+			switch(app_state)
+			{
+				
+				case PRINT_QUESTION:
+				{
+					if(Question[QUESTION2][quest_iterator] != '?')
+					{
+						//LCD_4Bits_Print_Character(1,quest_iterator,Question[QUESTION1][quest_iterator]);
+						LCD_Data_G[0][quest_iterator]= Question[QUESTION2][quest_iterator];
+						quest_iterator++;
+					}
+					else
+					{
+						app_state = PRINT_CHOICES;
+						quest_iterator = 0;
+					}
+
+					break;
+				}
+				
+				
+				case PRINT_CHOICES:
+				{
+					if(Display_choices[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+					{
+						LCD_Data_G[1][quest_iterator] = Display_choices[quest_iterator];
+						quest_iterator++;
+					}
+
+					else								 // Answer Statement has been completely printed so it's time to choose your answer
+					{
+						quest_iterator=0;
+						app_state = GET_ANSWER;
+					}
+
+					break;
+				}
+
+
+				case GET_ANSWER:
+				{
+					if(	Keypad_Get_Data_from_Buffer(&Answer) )
+					{
+						if(Answer == Answer_of_Questions[QUESTION2] )
+						{
+							Correct_Answer++;
+							correct_answer_flag = 1;
+							LCD_Send_Byte( 0x01 ,0);
+							app_state = SHOW_CORRECT_OR_NOT;
+							
+						}
+
+						else if(Answer != Answer_of_Questions[QUESTION2] && Answer != SKIP)
+						{
+							//clear screen
+							LCD_Send_Byte( 0x01 ,0);
+
+							wrong_answer_flag = 1;
+							app_state = SHOW_CORRECT_OR_NOT;
+
+						}
+						else if (Answer == SKIP)
+						{
+							//clear screen
+							LCD_Send_Byte( 0x01 ,0);
+							app_state = PRINT_QUESTION;
+							Question_State=QUESTION3;
+							//Do Nothing
+						}
+						else
+						{
+							//error not availble
+						}
+						
+						
+						quest_iterator=0;
+					}
+					break;
+				}
+				case SHOW_CORRECT_OR_NOT:
+				{
+					if(correct_answer_flag == 1 )
+					{
+						
+						//LCD_Send_Byte( 0x01 ,0);
+						if(score[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+						{
+							LCD_Data_G[0][quest_iterator] = right[quest_iterator];
+							quest_iterator++;
+						}
+						else
+						{
+							quest_iterator = 0;
+							//LCD_Send_Byte( 0x01 ,0);
+							correct_answer_flag = 0;
+							app_state = PRINT_QUESTION;
+							Question_State = QUESTION3;
+						}
+						
+						
+					}
+					else if (wrong_answer_flag == 1 )
+					{
+						if(score[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+						{
+							LCD_Data_G[0][quest_iterator] = wrong[quest_iterator];
+							quest_iterator++;
+						}
+						else
+						{
+							quest_iterator = 0;
+							wrong_answer_flag = 0;
+							app_state = PRINT_QUESTION;
+							Question_State = QUESTION3;
+						}
+					}
+					break;
+				}
+				
+			}
+			break;
+		}
+		case QUESTION3:
+		{
+			switch(app_state)
+			{
+				
+				case PRINT_QUESTION:
+				{
+					if(Question[QUESTION3][quest_iterator] != '?')
+					{
+						LCD_Data_G[0][quest_iterator]= Question[QUESTION3][quest_iterator];
+						quest_iterator++;
+					}
+					else
+					{
+						app_state = PRINT_CHOICES;
+						quest_iterator = 0;
+					}
+
+					break;
+				}
+				
+				
+				case PRINT_CHOICES:
+				{
+					if(Display_choices[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+					{
+						LCD_Data_G[1][quest_iterator] = Display_choices[quest_iterator];
+						quest_iterator++;
+					}
+
+					else								 // Answer Statement has been completely printed so it's time to choose your answer
+					{
+						quest_iterator=0;
+						app_state = GET_ANSWER;
+					}
+
+					break;
+				}
+
+
+				case GET_ANSWER:
+				{
+					if(	Keypad_Get_Data_from_Buffer(&Answer) )
+					{
+						if(Answer == Answer_of_Questions[QUESTION3] )
+						{
+							Correct_Answer++;
+							correct_answer_flag = 1;
+							wrong_answer_flag = 0;
+							LCD_Send_Byte( 0x01 ,0);
+							app_state = SHOW_CORRECT_OR_NOT;
+							
+						}
+
+						else if(Answer != Answer_of_Questions[QUESTION3] && Answer != SKIP)
+						{
+							//clear screen
+							LCD_Send_Byte( 0x01 ,0);
+
+							wrong_answer_flag = 1;
+							correct_answer_flag = 0;
+							app_state = SHOW_CORRECT_OR_NOT;
+
+						}
+						else if (Answer == SKIP)
+						{
+							//clear screen
+							LCD_Send_Byte( 0x01 ,0);
+							app_state = PRINT_QUESTION;
+							Question_State=QUESTION4;
+							//Do Nothing
+						}
+						else
+						{
+							//error not availble
+						}
+						quest_iterator=0;
+					}
+					break;
+				}
+				case SHOW_CORRECT_OR_NOT:
+				{
+					if(correct_answer_flag == 1 )
+					{
+						
+						//LCD_Send_Byte( 0x01 ,0);
+						if(score[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+						{
+							LCD_Data_G[0][quest_iterator] = right[quest_iterator];
+							quest_iterator++;
+						}
+						else
+						{
+							quest_iterator = 0;
+							correct_answer_flag = 0;
+							app_state = PRINT_QUESTION;
+							Question_State = QUESTION4;
+						}
+						
+						
+					}
+					else if (wrong_answer_flag == 1 )
+					{
+						if(score[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+						{
+							LCD_Data_G[0][quest_iterator] = wrong[quest_iterator];
+							quest_iterator++;
+						}
+						else
+						{
+							quest_iterator = 0;
+							wrong_answer_flag = 0;
+							app_state = PRINT_QUESTION;
+							Question_State = QUESTION4;
+						}
+					}
+					break;
+				}
+			}
+			break;
+		}
+		
+		
+		case QUESTION4:
+		{
+			switch(app_state)
+			{
+				
+				case PRINT_QUESTION:
+				{
+					if(Question[QUESTION4][quest_iterator] != '?')
+					{
+						
+						LCD_Data_G[0][quest_iterator]= Question[QUESTION4][quest_iterator];
+						quest_iterator++;
+					}
+					else
+					{
+						app_state = PRINT_CHOICES;
+						quest_iterator = 0;
+					}
+
+					break;
+				}
+				
+				
+				case PRINT_CHOICES:
+				{
+					if(Display_choices[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+					{
+						//LCD_4Bits_Print_Character(2,quest_iterator,Display_choices[quest_iterator]);
+						LCD_Data_G[1][quest_iterator] = Display_choices[quest_iterator];
+						quest_iterator++;
+					}
+
+					else								 // Answer Statement has been completely printed so it's time to choose your answer
+					{
+						quest_iterator=0;
+						app_state = GET_ANSWER;
+					}
+
+					break;
+				}
+
+
+				case GET_ANSWER:
+				{
+					if(	Keypad_Get_Data_from_Buffer(&Answer) )
+					{
+						if(Answer == Answer_of_Questions[QUESTION4] )
+						{
+							Correct_Answer++;
+							correct_answer_flag = 1;
+							LCD_Send_Byte( 0x01 ,0);
+							app_state = SHOW_CORRECT_OR_NOT;
+							
+						}
+
+						else if(Answer != Answer_of_Questions[QUESTION4] && Answer != SKIP)
+						{
+							//clear screen
+							LCD_Send_Byte( 0x01 ,0);
+
+							wrong_answer_flag = 1;
+							app_state = SHOW_CORRECT_OR_NOT;
+
+						}
+						else if (Answer == SKIP)
+						{
+							//clear screen
+							LCD_Send_Byte( 0x01 ,0);
+							app_state = PRINT_QUESTION;
+							Question_State=QUESTION5;
+							//Do Nothing
+						}
+						else
+						{
+							//error not availble
+						}
+						
+						
+						quest_iterator=0;
+					}
+					break;
+				}
+				case SHOW_CORRECT_OR_NOT:
+				{
+					if(correct_answer_flag == 1 )
+					{
+						
+						//LCD_Send_Byte( 0x01 ,0);
+						if(score[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+						{
+							LCD_Data_G[0][quest_iterator] = right[quest_iterator];
+							quest_iterator++;
+						}
+						else
+						{
+							quest_iterator = 0;
+							correct_answer_flag = 0;
+							app_state = PRINT_QUESTION;
+							Question_State = QUESTION5;
+						}
+						
+						
+					}
+					else if (wrong_answer_flag == 1 )
+					{
+						if(score[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+						{
+							LCD_Data_G[0][quest_iterator] = wrong[quest_iterator];
+							quest_iterator++;
+						}
+						else{
+							quest_iterator = 0;
+							wrong_answer_flag = 0;
+							app_state = PRINT_QUESTION;
+							Question_State = QUESTION5;
+							
+						}
+					}
+					break;
+				}
+			}
+			break;
+		}
+		
+		
+		case QUESTION5:
+		{
+			switch(app_state)
+			{
+				
+				case PRINT_QUESTION:
+				{
+					if(Question[QUESTION5][quest_iterator] != '?')
+					{
+						//LCD_4Bits_Print_Character(1,quest_iterator,Question[QUESTION1][quest_iterator]);
+						LCD_Data_G[0][quest_iterator]= Question[QUESTION5][quest_iterator];
+						quest_iterator++;
+					}
+					else
+					{
+						//LCD_Send_Byte( 0x01 ,0);
+						app_state = PRINT_CHOICES;
+						quest_iterator = 0;
+					}
+
+					break;
+				}
+				
+				
+				case PRINT_CHOICES:
+				{
+					if(Display_choices[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+					{
+						//LCD_4Bits_Print_Character(2,quest_iterator,Display_choices[quest_iterator]);
+						LCD_Data_G[1][quest_iterator] = Display_choices[quest_iterator];
+						quest_iterator++;
+					}
+
+					else								 // Answer Statement has been completely printed so it's time to choose your answer
+					{
+						//LCD_Send_Byte( 0x01 ,0);
+						quest_iterator=0;
+						app_state = GET_ANSWER;
+					}
+
+					break;
+				}
+
+
+				case GET_ANSWER:
+				{
+					if(	Keypad_Get_Data_from_Buffer(&Answer) )
+					{
+						if(Answer == Answer_of_Questions[QUESTION5] )
+						{
+							Correct_Answer++;
+							correct_answer_flag = 1;
+							LCD_Send_Byte( 0x01 ,0);
+							app_state = SHOW_CORRECT_OR_NOT;
+							
+						}
+
+						else if(Answer != Answer_of_Questions[QUESTION5] && Answer != SKIP)
+						{
+							//clear screen
+							LCD_Send_Byte( 0x01 ,0);
+
+							wrong_answer_flag = 1;
+							app_state = SHOW_CORRECT_OR_NOT;
+
+						}
+						else if (Answer == SKIP)
+						{
+							//clear screen
+							LCD_Send_Byte( 0x01 ,0);
+							//app_state = SHOW_CORRECT_OR_NOT;
+							Question_State = RESULT;
+							//Do Nothing
+						}
+						else
+						{
+							//error not availble
+						}
+						
+						
+						quest_iterator=0;
+					}
+					break;
+				}
+				case SHOW_CORRECT_OR_NOT:
+				{
+					if(fini == 0)
+					{
+						LCD_Send_Byte( 0x01 ,0);
+						fini = 1;
+					}
+					else if(fini == 1)
+					{
+						if(correct_answer_flag == 1 )
+						{
+							
+							//LCD_Send_Byte( 0x01 ,0);
+							if(score[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+							{
+								LCD_Data_G[0][quest_iterator] = right[quest_iterator];
+								quest_iterator++;
+							}
+							else
+							{
+								LCD_Send_Byte( 0x01 ,0);
+								quest_iterator = 0;
+								correct_answer_flag = 0;
+								Question_State = RESULT;
+							}
+						}
+						else if (wrong_answer_flag == 1 )
+						{
+							if(score[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+							{
+								LCD_Data_G[0][quest_iterator] = wrong[quest_iterator];
+								quest_iterator++;
+							}
+							else
+							{
+								LCD_Send_Byte( 0x01 ,0);
+								quest_iterator = 0;
+								wrong_answer_flag = 0;
+								Question_State = RESULT;
+							}
+						}
+
+					}
+					break;
+				}
+				
+			}
+			break;
+		}
+		case RESULT:
+		{
+			if(score[quest_iterator] != '\0')			// wait your answer choices to be completely displayed in the second line then select your answer through keypad
+			{
+				LCD_Data_G[0][quest_iterator] = score[quest_iterator];
+				quest_iterator++;
+			}
+			else
+			{
+				//LCD_Send_Byte(0x01,0);
+				LCD_Data_G[0][10]  = Correct_Answer + '0';
+			}
+			break;
+		}
+	}
+
+}
+
+
+
+
---
+++ questionHandl.h
@@ -0,0 +1,30 @@
+﻿/*
+ * IncFile1.h
+ *
+ * Created: 26/10/2019 08:18:52 م
+ *  Author: COMPUTER SHOW
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+#include "OS.h"
+#include "LCD.h"
+#include "Keypad.h"
+#include "DIO.h"
+#include "Timer.h"
+
+
+#define YES  '1'
+#define NO   '2'
+#define SKIP '3'
+
+
+typedef enum   { QUESTION1=0, QUESTION2 ,QUESTION3,QUESTION4,QUESTION5,RESULT} Question_State_t;
+typedef enum   { PRINT_QUESTION=0, PRINT_CHOICES ,GET_ANSWER,SHOW_CORRECT_OR_NOT} APP_STATE_t;
+	
+void Question_Handling_Task (void);
+
+
+#endif /* INCFILE1_H_ */
